7abb5db2bed97f665ff027b030bef06d7eaecd6d
Stavros Meskos <stameskos@gmail.com>
2020-12-01 23:16:28 +0200

    first commit

---
+++ Day_1/part_1.f90
@@ -0,0 +1,34 @@
+program part_1
+    implicit none
+    character(len=*), parameter :: fname = "expenses.input"
+    integer, allocatable :: numbers(:)
+    integer :: funit, io, n, i, j
+
+    open(newunit=funit, file=fname, action='read')
+    n = 0
+    do  
+        read(funit,*,iostat=io)
+        if ( io /= 0 ) exit
+        n = n +1
+    end do
+
+    allocate( numbers(n) )
+
+    rewind(funit)
+
+    read(funit,*) numbers(1)
+
+    do i = 2, n
+        read(funit,*) numbers(i)
+        do j = i-1, 1, -1
+            associate( ni=>numbers(i), nj=>numbers(j) )
+            if ( ni + nj == 2020 ) then
+                write(*,*) i, j, ni, nj, ni * nj 
+                stop
+            end if 
+            end associate
+        end do 
+    end do 
+
+
+end program part_1
---
+++ Day_1/part_2.f90
@@ -0,0 +1,37 @@
+program part_2
+    implicit none
+    character(len=*), parameter :: fname = "expenses.input"
+    integer, allocatable :: numbers(:)
+    integer :: funit, io, n, i, j, k
+
+    open(newunit=funit, file=fname, action='read')
+    n = 0
+    do  
+        read(funit,*,iostat=io)
+        if ( io /= 0 ) exit
+        n = n + 1
+    end do
+
+    allocate( numbers(n) )
+
+    rewind(funit)
+
+    read(funit,*) numbers(1) 
+    read(funit,*) numbers(2)
+
+    do i = 3, n
+        read(funit,*) numbers(i) 
+        do j = i-1, 1, -1
+            do k = j-1, 1, -1
+                associate( ni=>numbers(i), nj=>numbers(j), nk=>numbers(k) )
+                if ( ni + nj + nk == 2020 ) then
+                    write(*,*) i, j, k, ni, nj, nk, ni * nj *nk
+                    stop
+                end if 
+                end associate
+            end do
+        end do 
+    end do 
+
+
+end program part_2
